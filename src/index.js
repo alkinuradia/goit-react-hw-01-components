91b344c2e4dcdd5b687bcf589a51ddd60c288011
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-04-10 20:03:36 +0300

    туц

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode> 1221
     <App />
   </React.StrictMode>
 );
